99c7dd302faa8b33d508a0785bb0032ed34296db
Jesus Arredondo <arredondo16.jesus@gmail.com>
2021-09-07 22:51:26 -0700

    Cleaned up calculateMissingValue function and added comments.

--- RandomArray/Program.cs
+++ RandomArray/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//I was asked this question in an interview so I decided to write the solution I provided.
+//Question: You have an array with numbers 1-50, not in order and one value is replaced with 0.
+//How do you find which value is "missing" (replaced with 0)?
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,15 +15,19 @@ namespace RandomArray
             int arraySize = 50;
             int[] array = new int[arraySize];
             
+            //Populate array from numbers 1 to given arraySize
             array = fillArray(array, arraySize);
+
+            //Replace a random value with the number 0
             array = replaceArrayValue(array, arraySize);
-            printArray(array);
 
+            Console.WriteLine($"Missing Value: {calculateMissingValue(array)}");
+
+
+            //Print array before and after sorting to compare results with actual values
+            printArray(array);
             Array.Sort(array);
-
-            displayMissingValue(array);
-
-            printArray(array);
+            printArray(array);       
         }
 
         //Test Data
@@ -31,29 +39,32 @@ namespace RandomArray
             returnDictionary.Add(array1, array2);
             return returnDictionary;
         }
-
-        public static void displayMissingValue(int[] array)
+        
+        //Solution 1
+        //Find the missing value in the array by calculating the difference in actual vs expected array sums
+        public static int calculateMissingValue(int[] array)
         {
-            int difference = 0;
+            int expectedArraySum = 0;
             for (int i = array.Length; i > 0; i--)
-                difference += i;
+                expectedArraySum += i;
 
-            int sum = 0;
+            int actualArraySum = 0;
             for (int j = 0; j < array.Length; j++)
-                sum += array[j];
+                actualArraySum += array[j];
 
-           difference -= sum;
-           Console.WriteLine($"Missing number: {difference}");
+            return expectedArraySum - actualArraySum;
         }
 
-        public static int[] fillArray(int[] array, int total)
+        //Populeate the values in the array start at 1 and ending at the given array size
+        public static int[] fillArray(int[] array, int arraySize)
         {
-            for(int i = 0; i < total; i++)
+            for(int i = 0; i < arraySize; i++)
                 array[i] = i + 1;
 
             return shuffleArray(array);
         }
 
+        //Shuffle the order of the values in the array
         public static int[] shuffleArray(int[] array)
         {
             Random random = new Random();
@@ -61,6 +72,7 @@ namespace RandomArray
             return array;
         }
 
+        //Print out the values in the array in a single line
         public static void printArray(int[] array)
         {
             foreach (int number in array)
@@ -68,16 +80,18 @@ namespace RandomArray
             Console.WriteLine();
         }
 
-        public static int[] replaceArrayValue(int[] array, int total)
+        //Replace a value in the array with a 0
+        public static int[] replaceArrayValue(int[] array, int arraySize)
         {
-            array[chooseRandomIndex(total)] = 0;
+            array[chooseRandomIndex(arraySize)] = 0;
             return array;
         }
 
-        public static int chooseRandomIndex(int total)
+        //Random number generator used to select an index value in the array
+        public static int chooseRandomIndex(int arraySize)
         {
-            Random r = new Random();
-            return r.Next(0, total - 1);
+            Random random = new Random();
+            return random.Next(0, arraySize - 1);
         }
     }
 }
